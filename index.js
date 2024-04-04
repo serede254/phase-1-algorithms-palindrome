0bc6eca17fffb02e996a1bc2629fd4e74abd7f6d
root <root@SURFACE.>
2024-04-04 18:51:34 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var re = /[^a-zA-Z]/g;
+  var lowStr = word.toLowerCase().replace(re, '');
+  var reverseStr = lowStr.split('').reverse().join(''); 
+  return reverseStr === lowStr;
 }
 
 /* 
-  Add your pseudocode here
+  - remove spaces and special symbols from word
+  -convert to lowerCase
+  -reverse our string
+  -compare the two strings
 */
 
 /*
-  Add written explanation of your solution here
+ - we first make our string easier to handle by removing spaces and any special symbols
+ -we then create a new string made up of our new string
+ -we then split the new string into sub-strings and then reverse the index positions before rejoining them 
+ -we then compare the new and old strings to determine if it is a palindrome 
 */
 
 // You can run `node index.js` to view these console logs
